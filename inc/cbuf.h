27945fb29354c1da70478d83da6c760170f7426e
Danil Borchevkin <danil.borchevkin@gmail.com>
2024-10-15 04:03:11 +0300

    Refactor internals

--- inc/cbuf.h
+++ inc/cbuf.h
@@ -38,8 +38,8 @@
  *  @{
  */
 
-#ifndef __MODULE_H_
-#define __MODULE_H_
+#ifndef __CBUF_H_
+#define __CBUF_H_
 
 #ifdef __cplusplus
 extern "C" {
@@ -58,25 +58,28 @@ extern "C" {
  ******************************************************************************/
 
 #ifndef CONFIG_CBUF_MAX_LEN
-#define CONFIG_CBUF_MAX_LEN ((size_t)32)   /**< cbuf length. May be set in user code */
-#endif // CONFIG_CBUF_MAX_LEN
+#define CONFIG_CBUF_MAX_LEN \
+    ((size_t)32) /**< cbuf length. May be set in user code */
+#endif           // CONFIG_CBUF_MAX_LEN
 
 #ifndef CONFIG_CBUF_VAL_TYPE
-#define CONFIG_CBUF_VAL_TYPE int32_t /**< cbuf value types. May be set in user code */
-#endif // CONFIG_CBUF_VAL_TYPE
+#define CONFIG_CBUF_VAL_TYPE \
+    int32_t /**< cbuf value types. May be set in user code */
+#endif      // CONFIG_CBUF_VAL_TYPE
 
 #ifdef CONFIG_CBUF_TEST
 #define CBUF_STATIC
-#else 
+#else
 #define CBUF_STATIC static
 #endif
 
-#define CBUF_ERR_OK ((int32_t)0)      /**< cbuf error - no error */
-#define CBUF_ERR_PARAM ((int32_t)-1)   /**< cbuf error - error_param */
-#define CBUF_ERR_VALIDATION ((int32_t) -2) /**< cbuf error - validation */
-#define CBUF_ERR_OVERFLOW ((int32_t) -3) /**< cbuf error - overflow */
-#define CBUF_ERR_EMPTY      ((int32_t) -4) /**< cbuf error - empty */
-#define CBUF_ERR_NG         ((int32_t) -127) /**< cbuf error - general error */
+#define CBUF_ERR_OK ((int32_t)0)            /**< cbuf error - no error */
+#define CBUF_ERR_PARAM ((int32_t) - 1)      /**< cbuf error - error_param */
+#define CBUF_ERR_VALIDATION ((int32_t) - 2) /**< cbuf error - validation */
+#define CBUF_ERR_OVERFLOW ((int32_t) - 3)   /**< cbuf error - overflow */
+#define CBUF_ERR_EMPTY ((int32_t) - 4)      /**< cbuf error - empty */
+#define CBUF_ERR_FULL ((int32_t) -5)        /**< cbuf error - full */
+#define CBUF_ERR_NG ((int32_t) - 127)       /**< cbuf error - general error */
 
 /******************************************************************************
  * PUBLIC TYPES
@@ -85,8 +88,9 @@ extern "C" {
 typedef CONFIG_CBUF_VAL_TYPE cbuf_val_t;
 
 typedef struct cbuf_s {
-    size_t head_idx;        /**< Index of first element in buf */
-    size_t tail_idx;        /**< Indef of element in buf for next write */
+    size_t head_idx;  /**< Index of first element in buf */
+    size_t len;     /**< Count of elements */
+    size_t capacity; /**< Max count of elements */
     CONFIG_CBUF_VAL_TYPE buf[CONFIG_CBUF_MAX_LEN];
 } cbuf_t;
 
@@ -98,14 +102,15 @@ typedef struct cbuf_s {
  * PUBLIC FUNCTION PROTOTYPES
  ******************************************************************************/
 
-int32_t cbuf_init(cbuf_t * p_cbuf);
-int32_t cbuf_deinit(cbuf_t * p_cbuf);
-int32_t cbuf_push(cbuf_t * p_cbuf, const cbuf_val_t * p_val);
-int32_t cbuf_peek(const cbuf_t * const p_cbuf, cbuf_val_t * p_val);
-int32_t cbuf_pop(cbuf_t * p_cbuf, cbuf_val_t * p_val);
-int64_t cbuf_len(const cbuf_t * const p_cbuf);
-int64_t cbuf_capacity(const cbuf_t * const p_cbuf);
-int32_t cbuf_is_full(const cbuf_t * const p_cbuf);
+int32_t cbuf_init(cbuf_t* p_cbuf);
+int32_t cbuf_deinit(cbuf_t* p_cbuf);
+int32_t cbuf_push(cbuf_t* p_cbuf, const cbuf_val_t* p_val);
+int32_t cbuf_peek(const cbuf_t* const p_cbuf, cbuf_val_t* p_val);
+int32_t cbuf_pop(cbuf_t* p_cbuf, cbuf_val_t* p_val);
+int64_t cbuf_len(const cbuf_t* const p_cbuf);
+int64_t cbuf_capacity(const cbuf_t* const p_cbuf);
+int32_t cbuf_is_empty(const cbuf_t* const p_cbuf);
+int32_t cbuf_is_full(const cbuf_t* const p_cbuf);
 
 /******************************************************************************
  * END OF HEADER'S CODE
@@ -114,6 +119,6 @@ int32_t cbuf_is_full(const cbuf_t * const p_cbuf);
 }
 #endif
 
-#endif  // __MODULE_H_
+#endif  // __CBUF_H_
 
 /** @}*/
--- src/cbuf.c
+++ src/cbuf.c
@@ -97,11 +97,11 @@ CBUF_STATIC int32_t cbuf_check(const cbuf_t * const p_cbuf) {
         return CBUF_ERR_PARAM;
     }
 
-    if (p_cbuf->head_idx >= CONFIG_CBUF_MAX_LEN) {
+    if (p_cbuf->head_idx >= p_cbuf->capacity) {
         return CBUF_ERR_VALIDATION;
     }
 
-    if (p_cbuf->tail_idx >= CONFIG_CBUF_MAX_LEN) {
+    if (p_cbuf->len > p_cbuf->capacity) {
         return CBUF_ERR_VALIDATION;
     }
 
@@ -118,7 +118,8 @@ int32_t cbuf_init(cbuf_t * p_cbuf) {
     }
 
     p_cbuf->head_idx = 0;
-    p_cbuf->tail_idx = 0;
+    p_cbuf->len = 0;
+    p_cbuf->capacity = CONFIG_CBUF_MAX_LEN;
 
     int32_t err = cbuf_check(p_cbuf);
     if (CBUF_ERR_OK != err) {
@@ -149,17 +150,27 @@ int32_t cbuf_push(cbuf_t * p_cbuf, const cbuf_val_t * p_val) {
         return CBUF_ERR_PARAM;
     }
 
-    p_cbuf->buf[p_cbuf->tail_idx] = p_val;
+    err = cbuf_is_full(p_cbuf);
+    switch (err) {
+        case CBUF_ERR_OK:
 
-    int64_t ret = cbuf_capacity(p_cbuf);
-    if (CBUF_ERR_OK != ret) {
-        return (int32_t) ret;
-    } 
+            err = CBUF_ERR_OK;
+            break;
 
-    p_cbuf->tail_idx = (p_cbuf->tail_idx + 1) % ((size_t) ret);
-    if (p_cbuf->tail_idx == p_cbuf->head_idx)
+        case CBUF_ERR_FULL:
+            ++p_cbuf->head_idx;
+            err = CBUF_ERR_OVERFLOW;
+            break;
 
-    return CBUF_ERR_OK;
+        default:
+            return err;
+    }
+
+    size_t push_idx = (p_cbuf->head_idx + p_cbuf->len) % p_cbuf->capacity;
+    
+    p_cbuf->buf[push_idx] = *p_val;
+
+    return err;
 }
 
 int32_t cbuf_peek(const cbuf_t * const p_cbuf, cbuf_val_t * p_val) {
@@ -168,7 +179,18 @@ int32_t cbuf_peek(const cbuf_t * const p_cbuf, cbuf_val_t * p_val) {
         return err;
     }
 
-    // TODO finish this
+    if (NULL == p_val) {
+        return CBUF_ERR_PARAM;
+    }
+
+    err = cbuf_is_empty(p_cbuf);
+    if (CBUF_ERR_OK != err) {
+        return err;
+    }
+
+    size_t tail_idx = (p_cbuf->head_idx + p_cbuf->len - 1) % p_cbuf->capacity;
+
+    *p_val = p_cbuf->buf[tail_idx];
 
     return CBUF_ERR_OK;
 }
@@ -179,7 +201,17 @@ int32_t cbuf_pop(cbuf_t * p_cbuf, cbuf_val_t * p_val) {
         return err;
     }
 
-    // TODO finish this
+    err = cbuf_is_empty(p_cbuf);
+    if (CBUF_ERR_OK != err) {
+        return err;
+    }
+
+    err = cbuf_peek(p_cbuf, p_val);
+    if (CBUF_ERR_OK != err) {
+        return err;
+    }
+
+    --p_cbuf->len;
 
     return CBUF_ERR_OK;
 }
@@ -190,9 +222,7 @@ int64_t cbuf_len(const cbuf_t * const p_cbuf) {
         return err;
     }
 
-    // TODO finish this
-
-    return CBUF_ERR_OK;
+    return (int32_t) p_cbuf->len; 
 }
 
 int64_t cbuf_capacity(const cbuf_t * const p_cbuf) {
@@ -201,9 +231,20 @@ int64_t cbuf_capacity(const cbuf_t * const p_cbuf) {
         return err;
     }
 
-    // TODO finish this
+    return (int32_t) p_cbuf->capacity;
+}
 
-    return (int64_t) CONFIG_CBUF_MAX_LEN;
+int32_t cbuf_is_empty(const cbuf_t* const p_cbuf) {
+    int32_t err = cbuf_check(p_cbuf);
+    if (CBUF_ERR_OK != err) {
+        return err;
+    }
+
+    if (0 == p_cbuf->len) {
+        return CBUF_ERR_EMPTY;
+    }
+
+    return CBUF_ERR_OK;
 }
 
 int32_t cbuf_is_full(const cbuf_t * const p_cbuf) {
@@ -212,9 +253,11 @@ int32_t cbuf_is_full(const cbuf_t * const p_cbuf) {
         return err;
     }
 
-    // TODO finish this
+    if (p_cbuf->len == p_cbuf->capacity) {
+        return CBUF_ERR_FULL;
+    }
 
-    return CBUF_ERR_NG;
+    return CBUF_ERR_OK;
 }
 
 /******************************************************************************
